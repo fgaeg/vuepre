17d3111abaad41a7d213fee8651bc12ac08dbf6e
Bima Arafah <bima.arafah@kumparan.com>
2020-02-25 16:59:33 +0700

    add postProcess for prerender

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,14 @@ module.exports = {
       renderRoutes: ["/", "/about"],
       useRenderEvent: true,
       headless: true,
-      onlyProduction: true
+      onlyProduction: true,
+      postProcess: route => {
+        // Defer scripts and tell Vue it's been server rendered to trigger hydration
+        route.html = route.html
+          .replace(/<script (.*?)>/g, '<script $1 defer>')
+          .replace('id="app"', 'id="app" data-server-rendered="true"');
+        return route;
+      }
     }
   }
 };
