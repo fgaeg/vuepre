160ece8fe532186c4c72af7a299eef1cb4bf29f2
Bima Arafah <bima.arafah@kumparan.com>
2020-02-26 00:09:03 +0700

    add lint fix command

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,21 +1,8 @@
 <template>
   <div class="about p-3">
     <h1>This is an about page</h1>
-    <p v-if="buildTime">
-      <span id="intro">
-        Vue pre-rendered this page at <b>{{ buildTime }}</b> (before the browser ever saw it).
-      </span>
-    </p>
-    <template v-else>
-      <p>
-        Vue generated this page client-side because you navigated here from another route on the same site.
-      </p>
-      <p>
-        <a href="/about">Refresh the page</a> to see the pre-rendered version.
-      </p>
-    </template>
     <p>
-      The browser loaded this page at 
+      The browser loaded this page at
       <b>{{ loadTime }}</b>
     </p>
   </div>
@@ -23,15 +10,6 @@
 
 <script>
 export default {
-  asyncData() {
-    // Don't re-evaluate buildTime when the client loads this page in the
-    // browser.
-    if (!process.client) {
-      return {
-        buildTime: new Date()
-      };
-    }
-  },
   // Vue evaluates data variables at page render time and again every time the
   // browser loads this page.
   data: function() {
